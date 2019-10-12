1776278483a262f332657fa137005c6e6f2da073
yaotu <42772216+Zhouyaotu@users.noreply.github.com>
2019-10-12 23:58:07 +0800

    Revert "10.12"

--- mp-vue/src/router/index.js
+++ mp-vue/src/router/index.js
@@ -1,44 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// 导入刚才编写的组件
-import AppIndex from '@/components/home/AppIndex'
-import Login from '@/components/Login'
-import Home from '@/components/Home'
-import Tabs from '@/components/stulist/Tabs'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   routes: [
-  // 下面都是固定的写法
     {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/home',
-      name: 'Home',
-      component: Home,
-      // home页面并不需要被访问
-      redirect: '/index',
-      children: [
-      {
-        path: '/index',
-        name: 'AppIndex',
-        component: AppIndex,
-        meta: {
-          requireAuth: true
-        }
-      },
-      {
-        path: '/recommend',
-        name: 'Tabs',
-        component: Tabs
-      },
-      ]
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
-
